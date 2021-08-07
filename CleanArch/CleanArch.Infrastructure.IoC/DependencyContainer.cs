c3824df6e326cce6d22cb8b2b74f61623c8d6ca7
Abenezer Mulugeta <abenezermulugeta31@gmail.com>
2021-08-07 03:43:50 +0300

    Register services in IoC Container

--- CleanArch/CleanArch.Infrastructure.IoC/DependencyContainer.cs
+++ CleanArch/CleanArch.Infrastructure.IoC/DependencyContainer.cs
@@ -1,7 +1,13 @@
 ﻿using CleanArch.Application.Interfaces;
 using CleanArch.Application.Services;
+using CleanArch.Domain.CommandHandlers;
+using CleanArch.Domain.Commands;
+using CleanArch.Domain.Core.Bus;
 using CleanArch.Domain.Interfaces;
+using CleanArch.Infrastructure.Bus;
+using CleanArch.Infrastructure.Data.Context;
 using CleanArch.Infrastructure.Data.Repositories;
+using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
@@ -13,11 +19,20 @@ namespace CleanArch.Infrastructure.IoC
     {
         public static void RegisterServices(IServiceCollection services)
         {
+            // Domain InMemoryBus MediatR
+            services.AddScoped<IMediatorHandler, InMemoryBus>();
+
+            // Domain Commands Handlers
+            services.AddScoped<IRequestHandler<CreateCourseCommand, bool>, CourseCommandHandler>();
+
             // Application Layer
             services.AddScoped<ICourseService, CourseService>();
 
             // Infrastructure Layer
             services.AddScoped<ICourseRepository, CourseRepository>();
+
+            // MVC DbContext
+            services.AddScoped<UniversityDbContext>();
         }
     }
 }
